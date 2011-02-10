b941d99f6cd178820f9f5586b404dd46e1b937e7
xpansive <12600324>
2011-02-10 16:57:51 -0700

    Add Sheep.getColor and setColor

--- src/main/java/org/bukkit/entity/Sheep.java
+++ src/main/java/org/bukkit/entity/Sheep.java
@@ -3,6 +3,8 @@
  */
 package org.bukkit.entity;
 
+import org.bukkit.DyeColor;
+
 /**
  * Represents a Sheep.
  * 
@@ -11,4 +13,24 @@ package org.bukkit.entity;
  */
 public interface Sheep extends Animals {
 
+	/**
+	 * Gets the color of this sheep.
+	 * 
+	 * @return The DyeColor of this sheep.
+	 */
+	public DyeColor getColor();
+	
+	/**
+	 * Sets the color of this sheep to the specified data value.
+	 * 
+	 * @param color The color of the sheep, as a int.
+	 */
+	public void setColor(int color);
+	
+	/**
+	 * Sets the color of this sheep to the specified DyeColor.
+	 * 
+	 * @param color The color of the sheep, as a DyeColor.
+	 */
+	public void setColor(DyeColor color);
 }
