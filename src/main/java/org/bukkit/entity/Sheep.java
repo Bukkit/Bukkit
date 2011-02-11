7f0c9026c874e769676f72859ee8bbb62d5d71b0
xpansive <12600324>
2011-02-10 18:09:59 -0700

    Change tabs to spaces to abide coding conventions

--- src/main/java/org/bukkit/entity/Sheep.java
+++ src/main/java/org/bukkit/entity/Sheep.java
@@ -13,24 +13,24 @@ import org.bukkit.DyeColor;
  */
 public interface Sheep extends Animals {
 
-	/**
-	 * Gets the color of this sheep.
-	 * 
-	 * @return The DyeColor of this sheep.
-	 */
-	public DyeColor getColor();
-	
-	/**
-	 * Sets the color of this sheep to the specified data value.
-	 * 
-	 * @param color The color of the sheep, as a int.
-	 */
-	public void setColor(int color);
-	
-	/**
-	 * Sets the color of this sheep to the specified DyeColor.
-	 * 
-	 * @param color The color of the sheep, as a DyeColor.
-	 */
-	public void setColor(DyeColor color);
+    /**
+     * Gets the color of this sheep.
+     * 
+     * @return The DyeColor of this sheep.
+     */
+    public DyeColor getColor();
+    
+    /**
+     * Sets the color of this sheep to the specified data value.
+     * 
+     * @param color The color of the sheep, as a int.
+     */
+    public void setColor(int color);
+    
+    /**
+     * Sets the color of this sheep to the specified DyeColor.
+     * 
+     * @param color The color of the sheep, as a DyeColor.
+     */
+    public void setColor(DyeColor color);
 }
