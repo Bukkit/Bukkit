365e46aed9cedd6f71cced1dfafedc2cafc23d07
Raphfrk <raphfrk@gmail.com>
2011-02-12 15:53:26 +0000

    Player target methods

--- src/main/java/org/bukkit/entity/Player.java
+++ src/main/java/org/bukkit/entity/Player.java
@@ -2,8 +2,11 @@
 package org.bukkit.entity;
 
 import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.HashSet;
 import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
+import org.bukkit.block.Block;
 
 /**
  * Represents a player, connected or not
@@ -43,6 +46,35 @@ public interface Player extends HumanEntity, CommandSender {
      * @param loc
      */
     public void setCompassTarget(Location loc);
+
+     /**
+     * Gets all blocks along the player's line of sight
+     * List iterates from player's position to target inclusive
+     *
+     * @param HashSet<Byte> HashSet containing all transparent block ids
+     * @param int This is the maximum distance to scan.  This may be further limited by the server, but never to less than 100 blocks.
+     * @return List<Block> List containing all blocks along the player's line of sight
+     */
+    public List<Block> getLineOfSight(HashSet<Byte> transparent, int maxDistance);
+
+     /**
+     * Gets the block that the player has targetted
+     *
+     * @param HashSet<Byte> HashSet containing all transparent block ids
+     * @param int This is the maximum distance to scan.  This may be further limited by the server, but never to less than 100 blocks.
+     * @return Block Block that the player has targeted
+     */
+    public Block getTargetBlock(HashSet<Byte> transparent, int maxDistance);
+
+     /**
+     * Gets the last two blocks along the player's line of sight.
+     * The target block will be the last block in the list.
+     *
+     * @param HashSet<Byte> HashSet containing all transparent block ids
+     * @param int This is the maximum distance to scan.  This may be further limited by the server, but never to less than 100 blocks
+     * @return List containing the last 2 blocks along the player's line of sight
+     */
+    public List<Block> getLastTwoTargetBlocks(HashSet<Byte> transparent, int maxDistance);
 
     /**
      * Gets the socket address of this player
