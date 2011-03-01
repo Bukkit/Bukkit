61e18e01b740d8b1b990559acac505f5e828c61e
Tanel Suurhans <tanel.suurhans@perfectline.ee>
2011-03-02 00:35:41 +0200

    - implement boat damage - fix minecart damage event

--- src/main/java/org/bukkit/entity/Boat.java
+++ src/main/java/org/bukkit/entity/Boat.java
@@ -6,6 +6,23 @@ package org.bukkit.entity;
  * @author sk89q
  */
 public interface Boat extends Vehicle {
+
+    /**
+     * Sets the boat's damage
+     *
+     * @param damage over 40 to "kill" the boat
+     */
+    public void setDamage(int damage);
+
+
+    /**
+     * Gets the boat's damage
+     *
+     * @return damage int
+     */
+    public int getDamage();
+
+
     /**
      * Gets the maximum speed of a boat. The speed is unrelated to the velocity.
      *
