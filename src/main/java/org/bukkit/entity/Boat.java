8ed25630652448a9eaa81b03d5a434b8920fd29d
Tanel Suurhans <tanel.suurhans@perfectline.ee>
2011-03-02 01:09:30 +0200

    - implement boat damage

--- src/main/java/org/bukkit/entity/Boat.java
+++ src/main/java/org/bukkit/entity/Boat.java
@@ -6,6 +6,21 @@ package org.bukkit.entity;
  * @author sk89q
  */
 public interface Boat extends Vehicle {
+
+    /**
+     * Gets the boat's damage
+     *
+     * @return int damage
+     */
+    public int getDamage();
+
+    /**
+     * Sets the boat's damage
+     *
+     * @param damage int
+     */
+    public void setDamage(int damage);
+
     /**
      * Gets the maximum speed of a boat. The speed is unrelated to the velocity.
      *
