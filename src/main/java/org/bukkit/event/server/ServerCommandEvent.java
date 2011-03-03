6cf65d224a7ca2a6a0c339d0b1d16145dd83556e
Edwin Smulders <edwin.smulders@gmail.com>
2011-03-03 01:29:14 +0100

    ServerCommandEvent should get a commandname and arguments

--- src/main/java/org/bukkit/event/server/ServerCommandEvent.java
+++ src/main/java/org/bukkit/event/server/ServerCommandEvent.java
@@ -7,7 +7,21 @@ import org.bukkit.event.Event;
  * Server Command events
  */
 public class ServerCommandEvent extends Event {
-    public ServerCommandEvent(final Type type) {
+	
+	private String command;
+	private String[] args;
+	
+    public ServerCommandEvent(final Type type, String command, String[] args) {
         super(type);
+        this.command = command;
+        this.args = args;
+    }
+    
+    public String getCommand(){
+        return this.command;
+    }
+    
+    public String[] getArguments(){
+        return this.args;
     }
 }
