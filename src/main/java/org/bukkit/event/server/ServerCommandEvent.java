48ee0a385d6230c7e1700f93ce0fbbece144cf55
Edwin Smulders <edwin.smulders@gmail.com>
2011-03-03 01:36:02 +0100

    Add javadoc

--- src/main/java/org/bukkit/event/server/ServerCommandEvent.java
+++ src/main/java/org/bukkit/event/server/ServerCommandEvent.java
@@ -11,16 +11,30 @@ public class ServerCommandEvent extends Event {
 	private String command;
 	private String[] args;
 	
+	/**
+	 * Event for the call of server commands through the console
+	 * @param type The type of server command
+	 * @param command The command name
+	 * @param args the command arguments
+	 */
     public ServerCommandEvent(final Type type, String command, String[] args) {
         super(type);
         this.command = command;
         this.args = args;
     }
     
+    /**
+     * The name of this event's command.
+     * @return a commandname
+     */
     public String getCommand(){
         return this.command;
     }
     
+    /**
+     * The arguments of this event's command.
+     * @return the command arguments
+     */
     public String[] getArguments(){
         return this.args;
     }
