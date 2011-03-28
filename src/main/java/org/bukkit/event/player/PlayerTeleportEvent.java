76baf680d380d80698cb6bc74d1d657c89b44dc8
Byron Shelden <byron.shelden@gmail.com>
2011-03-27 18:32:43 -0700

    Cleaned up PlayerMoveEvent and PlayerTeleportEvent.  Now both inherit from a new PlayerChangePositionEvent.

--- src/main/java/org/bukkit/event/player/PlayerTeleportEvent.java
+++ src/main/java/org/bukkit/event/player/PlayerTeleportEvent.java
@@ -6,7 +6,14 @@ import org.bukkit.entity.Player;
 /**
  * Holds information for player teleport events
  */
-public class PlayerTeleportEvent extends PlayerMoveEvent {
+public class PlayerTeleportEvent extends PlayerChangePositionEvent {
+    /**
+     * Create a new player move event.
+     *
+     * @param player the player that's moving
+     * @param from the old location
+     * @param to the new location
+     */
     public PlayerTeleportEvent(Player player, Location from, Location to) {
         super(Type.PLAYER_TELEPORT, player, from, to);
     }
