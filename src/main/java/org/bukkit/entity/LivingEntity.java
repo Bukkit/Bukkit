c5540c2f8eac5bf071fcae42dcb3563815eebe2b
Robin van der Vliet <robin0van0der0vliet@hotmail.com>
2011-06-23 08:58:03 -0700

    Added shootFireball.

--- src/main/java/org/bukkit/entity/LivingEntity.java
+++ src/main/java/org/bukkit/entity/LivingEntity.java
@@ -94,6 +94,13 @@ public interface LivingEntity extends Entity {
     public Arrow shootArrow();
 
     /**
+     * Shoots an fireball from the entity.
+     *
+     * @return
+     */
+    public Arrow shootFireball();
+
+    /**
      * Returns whether this entity is inside a vehicle.
      *
      * @return
