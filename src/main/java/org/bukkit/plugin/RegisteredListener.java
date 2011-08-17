97f1501ec0edbe874c4e9482a9fd7c698a83257a
Layton Smith <laytonsmith14@gmail.com>
2011-08-17 17:41:03 -0500

    Made events stop propagating if they have been cancelled. Further work should be done to ensure that monitor level listeners don't actually edit the event further.

--- src/main/java/org/bukkit/plugin/RegisteredListener.java
+++ src/main/java/org/bukkit/plugin/RegisteredListener.java
@@ -55,6 +55,11 @@ public class RegisteredListener {
      * @return Registered Priority
      */
     public void callEvent(Event event) {
+        if(event instanceof Cancellable && this.priority != Event.Priority.Monitor){
+            if(((Cancellable)event).isCancelled()){
+                return;
+            }
+        }
         executor.execute(listener, event);
     }
 }
