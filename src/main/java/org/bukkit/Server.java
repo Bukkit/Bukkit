e6b75ab44f7c02defd86dd7870ba6128501fb847
Celtic Minstrel <celtic.minstrel.ca@some.place>
2011-08-30 02:54:29 -0400

    Eliminate a number of compiler warnings

--- src/main/java/org/bukkit/Server.java
+++ src/main/java/org/bukkit/Server.java
@@ -10,6 +10,8 @@ import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import java.util.logging.Logger;
+
+import org.bukkit.command.CommandException;
 import org.bukkit.command.PluginCommand;
 
 import org.bukkit.command.CommandSender;
