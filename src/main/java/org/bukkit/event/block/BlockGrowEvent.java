a5e1fbfefa691cb91d0d5ea536371ed4e7b8c83f
Ashwin Ganapathiraju <ag4ce@hotmail.com>
2011-10-11 18:18:28 -0400

    Added modifier methods for OldData and NewData. Silly me!

--- src/main/java/org/bukkit/event/block/BlockGrowEvent.java
+++ src/main/java/org/bukkit/event/block/BlockGrowEvent.java
@@ -1,35 +1,43 @@
- package org.bukkit.event.block;
+   package org.bukkit.event.block;
 
-import org.bukkit.block.Block;
-import org.bukkit.event.Event;
+   import org.bukkit.block.Block;
+   import org.bukkit.event.Event;
 
 /**
  * This event is called whenever a block tries to grow (just Crops for now.)
  */
-public class BlockGrowEvent extends BlockEvent {
-    protected int oldData;
-	 protected int newData;
-	 protected boolean cancelled = false;
-	 
-	 public BlockGrowEvent(Block block, int dataOld, int dataNew) {
-	   super(Event.BLOCK_GROW, block);
-		oldData = dataOld;
-		newData = dataNew;
-	 }
-	 
-	 public int getOldData() {
-	   return oldData;
-	 }
-	 
-	 public int getNewData() {
-	   return newData;
-	 }
-	 
-	 public boolean isCancelled() {
-      return cancelled;
-    }
-
-    public void setCancelled(boolean cancel) {
-      cancelled = cancel;
-    }
-}
+   public class BlockGrowEvent extends BlockEvent {
+      protected int oldData;
+      protected int newData;
+      protected boolean cancelled = false;
+    
+      public BlockGrowEvent(Block block, int dataOld, int dataNew) {
+         super(Event.BLOCK_GROW, block);
+         oldData = dataOld;
+         newData = dataNew;
+      }
+    
+      public int getOldData() {
+         return oldData;
+      }
+    
+      public int getNewData() {
+         return newData;
+      }
+      
+   	public void setNewData(int dataNew) {
+   		newData = dataNew;
+   	}
+   	
+   	public void setOldData(int dataOld) {
+   		oldData = dataOld;
+   	}
+    
+      public boolean isCancelled() {
+         return cancelled;
+      }
+   
+      public void setCancelled(boolean cancel) {
+         cancelled = cancel;
+      }
+   }
