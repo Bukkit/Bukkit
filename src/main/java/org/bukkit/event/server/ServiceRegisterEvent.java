e200134b78dedb6b41deae0dbe4b5383947f27ca
zml2008 <zach@zachsthings.com>
2011-11-12 20:22:30 -0800

    Added service register and unregister events. Addresses BUKKIT-816

---
+++ src/main/java/org/bukkit/event/server/ServiceRegisterEvent.java
@@ -0,0 +1,24 @@
+package org.bukkit.event.server;
+
+import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+/**
+ * This event is called when a service is registered.
+ */
+public class ServiceRegisterEvent extends ServiceEvent {
+
+    public ServiceRegisterEvent(RegisteredServiceProvider<?> registeredProvider) {
+        super(registeredProvider);
+    }
+
+    private static final HandlerList handlers = new HandlerList();
+
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
