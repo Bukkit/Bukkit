f40754daf54eab5f6555a617d0cd3bfcedd69f06
Ian Macali <ianmacalinao@gmail.com>
2011-11-25 21:20:12 -0600

    Added checking for if an item is a food item.

--- src/main/java/org/bukkit/Material.java
+++ src/main/java/org/bukkit/Material.java
@@ -386,7 +386,32 @@ public enum Material {
     public boolean isBlock() {
         return id < 256;
     }
-
+    
+    /**
+     * Checks if this Material is edible.
+     * 
+     * @return true if this Material is edible
+     */
+    public boolean isEdible() {
+        return equals(Material.BREAD)
+                || equals(Material.CAKE)
+                || equals(Material.COOKIE)
+                || equals(Material.MELON)
+                || equals(Material.MUSHROOM_SOUP)
+                || equals(Material.RAW_CHICKEN)
+                || equals(Material.COOKED_CHICKEN)
+                || equals(Material.RAW_BEEF)
+                || equals(Material.COOKED_BEEF)
+                || equals(Material.RAW_FISH)
+                || equals(Material.COOKED_FISH)
+                || equals(Material.PORK)
+                || equals(Material.GRILLED_PORK)
+                || equals(Material.APPLE)
+                || equals(Material.GOLDEN_APPLE)
+                || equals(Material.ROTTEN_FLESH)
+                || equals(Material.SPIDER_EYE);
+    }
+    
     /**
      * Attempts to get the Material with the given ID
      *
