ae0f46c4234e7058bf99e0fdbc963281a6fd8438
N3X15 <nexis@7chan.org>
2011-12-20 06:08:17 -0800

    Bukkit nitpicking over comment formatting.

--- src/main/java/org/bukkit/block/BrewingStand.java
+++ src/main/java/org/bukkit/block/BrewingStand.java
@@ -2,22 +2,23 @@ package org.bukkit.block;
 
 /**
  * Represents a brewing stand within Minecraft.
+ * 
  * @author N3X15 <nexis@7chan.org>
- *
+ * 
  */
 public interface BrewingStand extends BlockState, ContainerBlock {
-
-	/**
-	 * How much time is left in the brewing cycle
-	 * 
-	 * @return Brew Time
-	 */
-	int getBrewingTime();
-	
-	/**
-	 * Set the time left before brewing completes.
-	 * 
-	 * @param brewTime Brewing time
-	 */
-	void setBrewingTime(int brewTime);
+    
+    /**
+     * How much time is left in the brewing cycle
+     * 
+     * @return Brew Time
+     */
+    int getBrewingTime();
+    
+    /**
+     * Set the time left before brewing completes.
+     * 
+     * @param brewTime Brewing time
+     */
+    void setBrewingTime(int brewTime);
 }
