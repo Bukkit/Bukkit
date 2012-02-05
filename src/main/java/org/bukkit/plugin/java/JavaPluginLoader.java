70b7ebb4d4619205fecd3e12a35e6cdcda07bb8f
DjDCH <bukkit.coding@djdch.com>
2012-02-04 20:09:26 -0500

    Removed period character at the end of loading and unload plugin message. Fixed BUKKIT-699.

--- src/main/java/org/bukkit/plugin/java/JavaPluginLoader.java
+++ src/main/java/org/bukkit/plugin/java/JavaPluginLoader.java
@@ -1042,7 +1042,7 @@ public class JavaPluginLoader implements PluginLoader {
         }
 
         if (!plugin.isEnabled()) {
-            String message = String.format("[%s] Loading %s.", plugin.getDescription().getName(), plugin.getDescription().getFullName());
+            String message = String.format("[%s] Loading %s", plugin.getDescription().getName(), plugin.getDescription().getFullName());
             server.getLogger().info(message);
 
             JavaPlugin jPlugin = (JavaPlugin) plugin;
@@ -1071,7 +1071,7 @@ public class JavaPluginLoader implements PluginLoader {
         }
 
         if (plugin.isEnabled()) {
-            String message = String.format("[%s] Unloading %s.", plugin.getDescription().getName(), plugin.getDescription().getFullName());
+            String message = String.format("[%s] Unloading %s", plugin.getDescription().getName(), plugin.getDescription().getFullName());
             server.getLogger().info(message);
 
             server.getPluginManager().callEvent(new PluginDisableEvent(plugin));
