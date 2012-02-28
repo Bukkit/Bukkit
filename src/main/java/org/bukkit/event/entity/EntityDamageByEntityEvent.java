7b18d7f8b6afc3145040f535122dcb1daf946eb0
steffen <steffenb198@aol.com>
2012-02-28 23:49:07 +0100

    Implemented Critical API of Entity damaged to Entitys. ADDS Bukkit-543

--- src/main/java/org/bukkit/event/entity/EntityDamageByEntityEvent.java
+++ src/main/java/org/bukkit/event/entity/EntityDamageByEntityEvent.java
@@ -1,5 +1,7 @@
 package org.bukkit.event.entity;
 
+import java.util.Random;
+
 import org.bukkit.entity.Entity;
 
 /**
@@ -7,9 +9,15 @@ import org.bukkit.entity.Entity;
  */
 public class EntityDamageByEntityEvent extends EntityDamageEvent {
     private final Entity damager;
+    private boolean critical;
 
+    public EntityDamageByEntityEvent(final Entity damager, final Entity damagee, final DamageCause cause, final int damage, boolean critical) {
+        this(damager, damagee, cause, damage);
+        this.critical = critical;
+    }
+    
     public EntityDamageByEntityEvent(final Entity damager, final Entity damagee, final DamageCause cause, final int damage) {
-        super(damagee, cause, damage);
+    	super(damagee, cause, damage);
         this.damager = damager;
     }
 
@@ -20,5 +28,30 @@ public class EntityDamageByEntityEvent extends EntityDamageEvent {
      */
     public Entity getDamager() {
         return damager;
+    }
+    /**
+     * Returns the WHOLE damage
+     * @returns if crit, normal damage + crit damage else normal damage
+     */
+    @Override
+    public int getDamage() {
+    	if(critical) 
+    		return this.damage + new Random().nextInt(this.damage / 2 + 2);
+    	else
+    		return this.damage;
+    }
+    /**
+     * Sets whether this was an crit
+     * @param crit TRUE if this was an crit
+     */
+    public void setCritical(boolean crit) {
+    	this.critical = crit;
+    }
+    /**
+     * Whether this is an critical strike/hit
+     * @return Whether this is an crit
+     */
+    public boolean isCritical() {
+    	return this.critical;
     }
 }
--- src/main/java/org/bukkit/event/entity/EntityDamageEvent.java
+++ src/main/java/org/bukkit/event/entity/EntityDamageEvent.java
@@ -10,7 +10,7 @@ import org.bukkit.event.HandlerList;
  */
 public class EntityDamageEvent extends EntityEvent implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
-    private int damage;
+    protected int damage;
     private boolean cancelled;
     private final DamageCause cause;
 
