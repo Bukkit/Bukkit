eb2e72ce0b5f07b791aa463825a4af95baba462a
NodinChan <nodinchan@live.com>
2012-04-22 09:56:19 +0800

    Revert "Revert "Let's see if this fixes the part where Github thought I changed something""
    This reverts commit 773477313aaab20422936bb5a054969d2541ef12.

--- src/main/java/org/bukkit/event/entity/SheepRegrowWoolEvent.java
+++ src/main/java/org/bukkit/event/entity/SheepRegrowWoolEvent.java
@@ -8,34 +8,34 @@ import org.bukkit.event.HandlerList;
  * Called when a sheep regrows its wool
  */
 public class SheepRegrowWoolEvent extends EntityEvent implements Cancellable {
-    private static final HandlerList handlers = new HandlerList();
-    private boolean cancel;
+	private static final HandlerList handlers = new HandlerList();
+	private boolean cancel;
 
-    public SheepRegrowWoolEvent(final Sheep sheep) {
-        super(sheep);
-        this.cancel = false;
-    }
+	public SheepRegrowWoolEvent(final Sheep sheep) {
+		super(sheep);
+		this.cancel = false;
+	}
 
-    public boolean isCancelled() {
-        return cancel;
-    }
+	public boolean isCancelled() {
+		return cancel;
+	}
 
-    public void setCancelled(boolean cancel) {
-        this.cancel = cancel;
-    }
+	public void setCancelled(boolean cancel) {
+		this.cancel = cancel;
+	}
 
-    @Override
-    public Sheep getEntity() {
-        return (Sheep) entity;
-    }
+	@Override
+	public Sheep getEntity() {
+		return (Sheep) entity;
+	}
 
-    @Override
-    public HandlerList getHandlers() {
-        return handlers;
-    }
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
 
-    public static HandlerList getHandlerList() {
-        return handlers;
-    }
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
 
 }
