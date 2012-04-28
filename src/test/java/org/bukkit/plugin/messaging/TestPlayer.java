bd4cb862c9d05f1ffca3e55d8a3cedea560b1e09
Jedediah Smith <jedediah@silencegreys.com>
2012-04-28 01:56:30 -0400

    add API to control whether a player affects spawning of nearby mobs

--- src/test/java/org/bukkit/plugin/messaging/TestPlayer.java
+++ src/test/java/org/bukkit/plugin/messaging/TestPlayer.java
@@ -645,6 +645,14 @@ public class TestPlayer implements Player {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    public void setAffectsSpawning(boolean yes) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean getAffectsSpawning() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
     public boolean addPotionEffect(PotionEffect effect) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
