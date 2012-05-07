e7eecd3bf0ce0abe108e538dceb02e310e9364bd
Travis Ralston <travpc@gmail.com>
2012-05-06 21:36:07 -0600

    Added PlayerItemBreakEvent; Adds BUKKIT-1600

---
+++ src/main/java/org/bukkit/event/player/PlayerItemBreakEvent.java
@@ -0,0 +1,36 @@
+package org.bukkit.event.player;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * Fired when a player's item breaks (such as a shovel or flint and steel)
+ */
+public class PlayerItemBreakEvent extends PlayerEvent {
+    private static final HandlerList handlers = new HandlerList();
+    private final ItemStack brokenItem;
+
+    public PlayerItemBreakEvent(final Player player, final ItemStack brokenItem){
+        super(player);
+        this.brokenItem = brokenItem;
+    }
+
+    /**
+     * Gets the item that broke
+     * 
+     * @return The broken item
+     */
+    public ItemStack getBrokenItem(){
+        return brokenItem;
+    }
+
+    @Override
+    public HandlerList getHandlers(){
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList(){
+        return handlers;
+    }
+}
