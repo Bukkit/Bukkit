38a9954d74c8285e4b7f15f6157d03b7779911fe
Adam <effectsdude@gmail.com>
2012-05-22 16:04:36 -0400

    Added PlayerToggleFlyingEvent

---
+++ src/main/java/org/bukkit/event/player/PlayerToggleFlyingEvent.java
@@ -0,0 +1,45 @@
+package org.bukkit.event.player;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+
+/**
+ * Called when a player toggles their flying state
+ */
+public class PlayerToggleFlyingEvent extends PlayerEvent implements Cancellable {
+    private static final HandlerList handlers = new HandlerList();
+    private final boolean isFlying;
+    private boolean cancel = false;
+
+    public PlayerToggleFlyingEvent(final Player player, final boolean isFlying) {
+        super(player);
+        this.isFlying = isFlying;
+    }
+
+    /**
+     * Returns whether the player is now flying or not.
+     *
+     * @return flying state
+     */
+    public boolean isSneaking() {
+        return isFlying;
+    }
+
+    public boolean isCancelled() {
+        return cancel;
+    }
+
+    public void setCancelled(boolean cancel) {
+        this.cancel = cancel;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
