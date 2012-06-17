e4c688d714fd5e8b98ec28c35b708701d54d7e8b
Michael Fabian Dirks <xaymar@levelnull.de>
2012-06-17 19:08:47 +0300

    Update master

--- src/main/java/org/bukkit/plugin/PluginLoadOrder.java
+++ src/main/java/org/bukkit/plugin/PluginLoadOrder.java
@@ -12,4 +12,8 @@ public enum PluginLoadOrder {
      * Indicates that the plugin will be loaded after the first/default world was created
      */
     POSTWORLD
+    /**
+     * Indicates that the plugin will be loaded right before the Server is done with it's booting
+     */
+    DONE
 }
