fb33adf94b3d038afd78c410db10f688851f52d9
Streammz <robbygroot@quicknet.nl>
2012-08-02 15:03:42 +0200

    Forgot the coding style + javadocs.. oops

--- src/main/java/org/bukkit/event/block/BlockDispenseLiquidEvent.java
+++ src/main/java/org/bukkit/event/block/BlockDispenseLiquidEvent.java
@@ -6,9 +6,9 @@ import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 
 /**
- * Called when an item is dispensed from a block.
+ * Called when a liquid is dispensed from a block.
  * <p />
- * If a Block Dispense event is cancelled, the block will not dispense the item.
+ * If a Block Dispense Liquid event is cancelled, the block will not dispense the item.
  */
 public class BlockDispenseLiquidEvent extends BlockEvent implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
@@ -31,19 +31,31 @@ public class BlockDispenseLiquidEvent extends BlockEvent implements Cancellable 
     public void setCancelled(boolean cancel) {
         cancelled = cancel;
     }
-    
+
+    /**
+     * Returns the material of the dispensed liquid.
+     * This should be Material.WATER_BUCKET or Material.LAVA_BUCKET
+     * 
+     * @return The material of the dispensed liquid
+     */
     public Material getMaterial() {
     	return material;
     }
-    
+
+    /**
+     * @return The block where the liquid will be placed.
+     */
     public Block getLiquidBlock() {
     	return liquidBlock;
     }
-    
+
+    /**
+     * @return Returns whether the dispenser is dispensing or sucking.
+     */
     public boolean isDispensing() {
     	return dispensing;
     }
-    
+
     @Override
     public HandlerList getHandlers() {
         return handlers;
