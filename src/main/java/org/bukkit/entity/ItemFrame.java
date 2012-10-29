186936a1d9c5d3f67ed34577336f0136695abba2
h31ix <effectsdude@gmail.com>
2012-10-29 02:25:42 -0400

    Add API for ItemFrames and reorder common Hanging/Attachable functions
    As well as adding methods for ItemFrames, this moves several values previously contained in Painting to Hanging, as they will now be shared by both classes.

--- src/main/java/org/bukkit/entity/ItemFrame.java
+++ src/main/java/org/bukkit/entity/ItemFrame.java
@@ -1,6 +1,65 @@
 package org.bukkit.entity;
 
+import org.bukkit.Material;
+
 /**
  * Represents an Item Frame
  */
-public interface ItemFrame extends Hanging {}
+public interface ItemFrame extends Hanging {
+    /**
+     * An enum to specify the rotation of the ItemFrame's item
+     */
+    public enum Rotation {
+        /**
+         * No rotation
+         */
+        NONE,
+        /**
+         * Rotated clockwise
+         */
+        CLOCKWISE,
+        /**
+         * Flipped upside-down
+         */
+        FLIPPED,
+        /**
+         * Rotated counter-clockwise
+         */
+        COUNTER_CLOCKWISE,
+    }
+
+    /**
+     * Get the item in this frame
+     *
+     * @return The item
+     */
+    public Material getItem();
+
+    /**
+     * Set the item in this frame
+     *
+     * @param item The new item
+     */
+    public void setItem(Material item);
+
+    /**
+     * Get the rotation of the frame's item
+     *
+     * @return The direction
+     */
+    public Rotation getRotation();
+
+    /**
+     * Set the rotation of the frame's item
+     *
+     * @param rotation The new rotation
+     */
+    public void setRotation(Rotation rotation);
+
+    /**
+     * Rotate the frame's item as if it were being right-clicked
+     *
+     * @return The new rotation
+     */
+    public Rotation rotate();
+}
