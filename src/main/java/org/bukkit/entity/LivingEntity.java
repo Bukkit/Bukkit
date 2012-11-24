371b615377e0b670e68854a015ac63b1b26d66e7
Olof Larsson <olof@sylt.nu>
2012-11-24 13:52:40 +0100

    Add equipment API for EntityLiving.

--- src/main/java/org/bukkit/entity/LivingEntity.java
+++ src/main/java/org/bukkit/entity/LivingEntity.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.bukkit.Location;
 import org.bukkit.block.Block;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -278,4 +279,201 @@ public interface LivingEntity extends Entity {
      * @return true if there is a line of sight, false if not.
      */
     public boolean hasLineOfSight(Entity other);
+    
+    /**
+     * Get the ItemStack from the specified equipment slot.<br />
+     * <br />
+     * <UL>
+     * <LI>Index 0 is Held</LI>
+     * <LI>Index 1 is Boots</LI>
+     * <LI>Index 2 is Leggings</LI>
+     * <LI>Index 3 is Chestplate</LI>
+     * <LI>Index 4 is Helmet</LI>
+     * </UL>
+     *
+     * @return The ItemStack from the specified equipment slot
+     * @param index The index of the equipment slot
+     */
+    public ItemStack getEquipment(int index);
+    
+    /**
+     * Get the ItemStack from the specified armor slot.<br />
+     * <br />
+     * <UL>
+     * <LI>Index 0 is Boots</LI>
+     * <LI>Index 1 is Leggings</LI>
+     * <LI>Index 2 is Chestplate</LI>
+     * <LI>Index 3 is Helmet</LI>
+     * </UL>
+     *
+     * @return The ItemStack from the specified armor slot
+     * @param index The index of the armor slot
+     */
+    public ItemStack getArmor(int index);
+    
+    /**
+     * Get all ItemStacks from the equipment slots.<br />
+     * <br />
+     * <UL>
+     * <LI>Index 0 is Held</LI>
+     * <LI>Index 1 is Boots</LI>
+     * <LI>Index 2 is Leggings</LI>
+     * <LI>Index 3 is Chestplate</LI>
+     * <LI>Index 4 is Helmet</LI>
+     * </UL>
+     *
+     * @return All the ItemStacks from the equipment slots
+     */
+    public ItemStack[] getEquipmentContents();
+    
+    /**
+     * Get all ItemStacks from the armor slots.<br />
+     * <br />
+     * <UL>
+     * <LI>Index 0 is Boots</LI>
+     * <LI>Index 1 is Leggings</LI>
+     * <LI>Index 2 is Chestplate</LI>
+     * <LI>Index 3 is Helmet</LI>
+     * </UL>
+     *
+     * @return All the ItemStacks from the armor slots
+     */
+    public ItemStack[] getArmorContents();
+
+    /**
+     * Return the ItemStack from the helmet slot
+     *
+     * @return The ItemStack in the helmet slot
+     */
+    public ItemStack getHelmet();
+
+    /**
+     * Return the ItemStack from the chestplate slot
+     *
+     * @return The ItemStack in the chestplate slot
+     */
+    public ItemStack getChestplate();
+
+    /**
+     * Return the ItemStack from the leggings slot
+     *
+     * @return The ItemStack in the leggings slot
+     */
+    public ItemStack getLeggings();
+
+    /**
+     * Return the ItemStack from the boots slot
+     *
+     * @return The ItemStack in the boots slot
+     */
+    public ItemStack getBoots();
+    
+    /**
+     * Put the ItemStack into the specified equipment slot.<br />
+     * The old item will be deleted.<br />
+     * <br />
+     * <UL>
+     * <LI>Index 0 is Held</LI>
+     * <LI>Index 1 is Boots</LI>
+     * <LI>Index 2 is Leggings</LI>
+     * <LI>Index 3 is Chestplate</LI>
+     * <LI>Index 4 is Helmet</LI>
+     * </UL>
+     *
+     * @param index The index of the equipment slot
+     * @param item The ItemStack to put there
+     */
+    public void setEquipment(int index, ItemStack item);
+    
+    /**
+     * Put the ItemStack into the specified armor slot.<br />
+     * The old item will be deleted.<br />
+     * <br />
+     * <UL>
+     * <LI>Index 0 is Boots</LI>
+     * <LI>Index 1 is Leggings</LI>
+     * <LI>Index 2 is Chestplate</LI>
+     * <LI>Index 3 is Helmet</LI>
+     * </UL>
+     *
+     * @param index The index of the armor slot
+     * @param item The ItemStack to put there
+     */
+    public void setArmor(int index, ItemStack item);
+    
+    /**
+     * Put the given ItemStacks into the equipment slots.<br />
+     * <br />
+     * <UL>
+     * <LI>Index 0 is Held</LI>
+     * <LI>Index 1 is Boots</LI>
+     * <LI>Index 2 is Leggings</LI>
+     * <LI>Index 3 is Chestplate</LI>
+     * <LI>Index 4 is Helmet</LI>
+     * </UL>
+     *
+     * @param items The ItemStacks to use as equipment
+     */
+    public void setEquipmentContents(ItemStack[] items);
+    
+    /**
+     * Put the given ItemStacks into the armor slots<br />
+     * <br />
+     * <UL>
+     * <LI>Index 0 is Boots</LI>
+     * <LI>Index 1 is Leggings</LI>
+     * <LI>Index 2 is Chestplate</LI>
+     * <LI>Index 3 is Helmet</LI>
+     * </UL>
+     *
+     * @param items The ItemStacks to use as armor
+     */
+    public void setArmorContents(ItemStack[] items);
+
+    /**
+     * Put the given ItemStack into the helmet slot
+     * This does not check if the ItemStack is a helmet
+     *
+     * @param helmet The ItemStack to use as helmet
+     */
+    public void setHelmet(ItemStack helmet);
+
+    /**
+     * Put the given ItemStack into the chestplate slot
+     * This does not check if the ItemStack is a chestplate
+     *
+     * @param chestplate The ItemStack to use as chestplate
+     */
+    public void setChestplate(ItemStack chestplate);
+
+    /**
+     * Put the given ItemStack into the leg slot
+     * This does not check if the ItemStack is a pair of leggings
+     *
+     * @param leggings The ItemStack to use as leggings
+     */
+    public void setLeggings(ItemStack leggings);
+
+    /**
+     * Put the given ItemStack into the boots slot
+     * This does not check if the ItemStack is a boots
+     *
+     * @param boots The ItemStack to use as boots
+     */
+    public void setBoots(ItemStack boots);
+    
+    /**
+     * Returns the ItemStack currently in the hand of the entity, can be empty.
+     * @return The ItemStack of the item the entity currently is holding.
+     *
+     */
+    public ItemStack getItemInHand();
+
+    /**
+     * Sets the item to the given ItemStack, this will replace whatever the
+     * entity was holding.
+     *
+     * @param item The ItemStack which will end up in the hand
+     */
+    public void setItemInHand(ItemStack item);
 }
