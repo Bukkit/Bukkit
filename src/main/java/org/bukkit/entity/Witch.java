f4a9ae42291ec30f920b55262f48de6b8aa94f6e
Steveice10 <Steveice10@gmail.com>
2012-12-11 06:19:19 -0800

    Expanded upon Witch API. Fixes BUKKIT-3147

--- src/main/java/org/bukkit/entity/Witch.java
+++ src/main/java/org/bukkit/entity/Witch.java
@@ -4,4 +4,17 @@ package org.bukkit.entity;
  * Represents a Witch
  */
 public interface Witch extends Monster {
+    /**
+     * Gets whether the witch is aggressive towards the player.
+     * 
+     * @return The witch's aggressiveness.
+     */
+    public boolean isAggressive();
+    
+    /**
+     * Sets whether the witch is aggressive towards the player.
+     * 
+     * @param Whether the witch should be aggressive or not.
+     */
+    public void setAggressive(boolean aggressive);
 }
