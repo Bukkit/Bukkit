83eff25481b4b99adf7dfb949a65999ce594432b
Olof Larsson <olof@sylt.nu>
2012-12-17 09:45:56 +0100

    Set/Get ItemStack from ThrownPotion. Adds BUKKIT-3197

--- src/main/java/org/bukkit/entity/ThrownPotion.java
+++ src/main/java/org/bukkit/entity/ThrownPotion.java
@@ -2,6 +2,7 @@ package org.bukkit.entity;
 
 import java.util.Collection;
 
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 
 /**
@@ -13,4 +14,21 @@ public interface ThrownPotion extends Projectile {
      * @return The potion effects
      */
     public Collection<PotionEffect> getEffects();
+
+    /**
+     * Returns a copy of the item stack for this thrown potion.
+     * Altering this copy will not alter the thrown potion directly.
+     * If you want to alter the thrown potion you must use the {@link #setItemStack(ItemStack) setItemStack} method.
+     * 
+     * @return A copy of the item stack for this thrown potion.
+     */
+    public ItemStack getItemStack();
+
+    /**
+     * Set the item stack for this thrown potion.
+     * The ItemStack must be a potion. Otherwise an exception is thrown.
+     * 
+     * @param itemStack 
+     */
+    public void setItemStack(ItemStack itemStack);
 }
