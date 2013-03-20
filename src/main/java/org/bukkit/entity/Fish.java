c6c12d3f03df3b5f3641c3e052e769ac66964593
Andre LeBlanc <andre@norcode.com>
2013-03-20 18:18:59 -0400

    added getters and setters for Bite-Chance.

--- src/main/java/org/bukkit/entity/Fish.java
+++ src/main/java/org/bukkit/entity/Fish.java
@@ -8,4 +8,8 @@ public interface Fish extends Projectile {
      * causes a fish to bite the hook. 
      */
     public void bite();
+    public short getBiteChance();
+    public void setBiteChance(short chance);
+    public short getRainyBiteChance();
+    public void setRainyBiteChance(short chance);
 }
