f7d1af8e61c165354898fcbc5cf004169e5ea883
Xephi <xephirot59@gmail.com>
2013-03-20 03:23:37 +0100

    Revert "Implement Dropper Interface - Change Javadoc"
    This reverts commit 300f0ded982022398b26e4761b4f3ee05b3bcb00.

--- src/main/java/org/bukkit/block/Dropper.java
+++ src/main/java/org/bukkit/block/Dropper.java
@@ -1,17 +1,16 @@
 package org.bukkit.block;
-//CraftBukkit start
+
 /**
  * Represents a dropper.
  */
 public interface Dropper extends Dispenser {
 
     /**
-     * Try to drop one of item from one itemstack the dropper contain
+     * Attempts to dispense the contents of this block
      *
-     * This dispense method return false if the block is no longer a dropper
+     * If the block is no longer a dropper, this will return false
      *
      * @return true if successful, otherwise false
      */
     public boolean dispense();
 }
-//CraftBukkit end
