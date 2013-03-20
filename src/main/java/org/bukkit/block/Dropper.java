300f0ded982022398b26e4761b4f3ee05b3bcb00
Xephi <xephirot59@gmail.com>
2013-03-20 02:34:34 +0100

    Implement Dropper Interface - Change Javadoc

--- src/main/java/org/bukkit/block/Dropper.java
+++ src/main/java/org/bukkit/block/Dropper.java
@@ -1,16 +1,17 @@
 package org.bukkit.block;
-
+//CraftBukkit start
 /**
  * Represents a dropper.
  */
 public interface Dropper extends Dispenser {
 
     /**
-     * Attempts to dispense the contents of this block
+     * Try to drop one of item from one itemstack the dropper contain
      *
-     * If the block is no longer a dropper, this will return false
+     * This dispense method return false if the block is no longer a dropper
      *
      * @return true if successful, otherwise false
      */
     public boolean dispense();
 }
+//CraftBukkit end
