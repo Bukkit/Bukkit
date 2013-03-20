5e3bb9722139bb782f1c7ac6602d9e2434a24217
Xephi <xephirot59@gmail.com>
2013-03-20 06:45:05 +0100

    Hopper !

--- src/main/java/org/bukkit/block/Dropper.java
+++ src/main/java/org/bukkit/block/Dropper.java
@@ -17,7 +17,7 @@ public interface Dropper extends BlockState, ContainerBlock {
      * 
      * Dropper can drop one by one these items : 1 3 2 7 1 2 2 3 4 6 6 ...
      * 
-     * If a container is around the dropper, this item go inside (like with a Hooper)
+     * If a container is around the dropper, this item go inside (like with a Hopper)
      *
      * @return true if successful drop an item, false if the block is no longer a dropper
      */
