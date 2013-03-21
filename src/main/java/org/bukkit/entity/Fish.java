2a50938ecfe710152d65fc0f9cc14f4533e39992
Andre LeBlanc <andre@norcode.com>
2013-03-20 22:39:16 -0400

    added javadocs

--- src/main/java/org/bukkit/entity/Fish.java
+++ src/main/java/org/bukkit/entity/Fish.java
@@ -4,12 +4,37 @@ package org.bukkit.entity;
  * Represents a fishing hook.
  */
 public interface Fish extends Projectile {
+
     /**
-     * causes a fish to bite the hook. 
+     * causes a fish to bite the hook.
      */
     public void bite();
+
+    /**
+     * Gets the chance of a fish biting during normal weather.
+     *
+     * @return chance
+     */
     public short getBiteChance();
+
+    /**
+     * Sets the chance of a fish biting during normal weather.
+     *
+     * @param chance "1-in-chance" chance of triggering a bite each tick
+     */
     public void setBiteChance(short chance);
+
+    /**
+     * Gets the chance of a fish biting outdoors during rain.
+     *
+     * @return chance
+     */
     public short getRainyBiteChance();
+
+    /**
+     * Sets the chance of a fish biting outdoors during rain.
+     *
+     * @param chance - "1-in-chance" chance chance of triggering a bite each tick
+     */
     public void setRainyBiteChance(short chance);
 }
