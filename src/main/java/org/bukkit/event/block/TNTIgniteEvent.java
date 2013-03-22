c84b1275ec9e76e2fdef031d118c84636bd83bad
nitnelave <nitnelave1@gmail.com>
2013-03-22 14:04:24 +0100

    [B+C] Added TNTIgniteEvent. Fixed BUKKIT-770
    TNTIgniteEvent is fired when a TNT block turns into an entity (when it gets primed).
    If it is cancelled, then the primed TNT isn't spawned.

---
+++ src/main/java/org/bukkit/event/block/TNTIgniteEvent.java
@@ -0,0 +1,44 @@
+package org.bukkit.event.block;
+
+import org.bukkit.block.Block;
+import org.bukkit.entity.TNTPrimed;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+
+/**
+ * Called when a TNT is ignited, leading to the creation of a primed TNT.
+ * <p />
+ * If a TNT Ignite Event is cancelled, the primed TNT will not be spawned.
+ */
+public class TNTIgniteEvent extends BlockEvent implements Cancellable {
+    private static final HandlerList handlers = new HandlerList();
+    private final TNTPrimed tntprimed;
+    private boolean cancelled;
+
+    public TNTIgniteEvent(final Block block, final TNTPrimed entity) {
+        super (block);
+        this.tntprimed = entity;
+    }
+
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    public void setCancelled(boolean cancel) {
+        this.cancelled = cancel;
+
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+    
+    public TNTPrimed getEntity() {
+        return tntprimed;
+    }
+}
