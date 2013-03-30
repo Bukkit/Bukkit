c0ad7901505066791e0644de65a8e78b599cd5d3
Daniel Naylor <git@drnaylor.co.uk>
2013-03-30 11:26:00 +0000

    Allow plugins to obtain the reason a player disconnected. Adds BUKKIT-429 Adds a QuitReason enum, and adds getQuitReason() to the PlayerQuitEvent and PlayerKickEvent. If a disconnect reason appears that does not feature in the enum, it falls back to "QuitReason.UNKNOWN" or "QuitReason.KICKED". A fallback constructor (marked as Deprecated) remains in place, just in case someone is calling the event from their plugins, this defaults to "QuitReason.UNKNOWN" or "QuitReason.KICKED".

--- src/main/java/org/bukkit/event/player/PlayerKickEvent.java
+++ src/main/java/org/bukkit/event/player/PlayerKickEvent.java
@@ -1,5 +1,6 @@
 package org.bukkit.event.player;
 
+import org.bukkit.QuitReason;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
@@ -12,11 +13,21 @@ public class PlayerKickEvent extends PlayerEvent implements Cancellable {
     private String leaveMessage;
     private String kickReason;
     private Boolean cancel;
+    private QuitReason reason;
 
+    /**
+     * @deprecated A new parameter has been included, QuitReason
+     */
+    @Deprecated
     public PlayerKickEvent(final Player playerKicked, final String kickReason, final String leaveMessage) {
+        this(playerKicked, kickReason, leaveMessage, QuitReason.KICKED);
+    }
+
+    public PlayerKickEvent(final Player playerKicked, final String kickReason, final String leaveMessage, final QuitReason reason) {
         super(playerKicked);
         this.kickReason = kickReason;
         this.leaveMessage = leaveMessage;
+        this.reason = reason;
         this.cancel = false;
     }
 
@@ -30,7 +41,18 @@ public class PlayerKickEvent extends PlayerEvent implements Cancellable {
     }
 
     /**
-     * Gets the leave message send to all online players
+     * Gets the reason the server kicked the player. This 
+     * method returns an enum, intended for use by other
+     * plugins. Use getReason() to obtain a text version.
+     *
+     * @return QuitReason reason
+     */
+    public QuitReason getQuitReason() {
+        return reason;
+    }
+
+    /**
+     * Gets the leave message sent to all online players
      *
      * @return string kick reason
      */
