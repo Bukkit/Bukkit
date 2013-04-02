d5a1bb22e33b6511cfc878fe9762d2527b42c409
riking <rikingcoding@gmail.com>
2013-04-01 19:12:04 -0700

    Add ItemDeathEvent. Adds BUKKIT-1900
    ItemDeathEvent is called when a dropped item recieves fatal damage that would cause it to be removed.

---
+++ src/main/java/org/bukkit/event/entity/ItemDeathEvent.java
@@ -0,0 +1,55 @@
+package org.bukkit.event.entity;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Item;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+
+/**
+ * This event is called when a {@link org.bukkit.entity.Item} receives damage
+ * and is about to be removed.
+ * <p>
+ * This event will never be called when a Item of
+ * {@link org.bukkit.Material#NETHER_STAR} is hurt by an explosion.
+ */
+public class ItemDeathEvent extends EntityEvent {
+    private static final HandlerList handlers = new HandlerList();
+    private final Location location;
+    private final DamageCause cause;
+
+    public ItemDeathEvent(final Item despawnee, final Location loc, final DamageCause cause) {
+        super(despawnee);
+        location = loc;
+        this.cause = cause;
+    }
+
+    @Override
+    public Item getEntity() {
+        return (Item) entity;
+    }
+
+    /**
+     * Gets the location at which the item is dying.
+     *
+     * @return The location at which the item is dying
+     */
+    public Location getLocation() {
+        return location;
+    }
+
+    /**
+     * Gets the source of the damage that killed this Item
+     */
+    public DamageCause getCause() {
+        return cause;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
