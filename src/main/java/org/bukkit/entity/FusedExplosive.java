6625c5a1ad8c1d861a305f7ee04a2fae29f6b22e
nitnelave <nitnelave1@gmail.com>
2013-04-02 16:39:44 +0200

    Updated documentation.

--- src/main/java/org/bukkit/entity/FusedExplosive.java
+++ src/main/java/org/bukkit/entity/FusedExplosive.java
@@ -5,17 +5,16 @@ package org.bukkit.entity;
  */
 public interface FusedExplosive extends Explosive {
     /**
-     * Set the number of ticks until the TNT blows up after being primed.
+     * Set the number of ticks until the entity blows up after being primed.
      *
      * @param fuseTicks The fuse ticks
      */
     public void setFuseTicks(int fuseTicks);
 
     /**
-     * Retrieve the number of ticks until the explosion of this TNTPrimed
-     * entity
+     * Retrieve the number of ticks until the explosion of this entity.
      *
-     * @return the number of ticks until this TNTPrimed explodes
+     * @return the number of ticks until this entity explodes
      */
     public int getFuseTicks();
 
