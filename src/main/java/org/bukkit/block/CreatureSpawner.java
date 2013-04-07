f17c9098d3eea5daf3b4bd7be4cf9c46b8d3a0e4
Jacek <jacek@kuzemczak.co.uk>
2013-04-07 16:43:51 +0100

    Add API for new mob spawner features. Adds BUKKIT-1998

--- src/main/java/org/bukkit/block/CreatureSpawner.java
+++ src/main/java/org/bukkit/block/CreatureSpawner.java
@@ -1,6 +1,7 @@
 package org.bukkit.block;
 
 import org.bukkit.entity.CreatureType;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 
 /**
@@ -30,6 +31,21 @@ public interface CreatureSpawner extends BlockState {
      * @param creatureType The creature type.
      */
     public void setSpawnedType(EntityType creatureType);
+
+    /**
+     * Sets the entity that will be spawned.
+     * 
+     * @param entity The entity
+     */
+    public void setSpawnedEntity(Entity entity);
+
+    /**
+     * Sets the entity that will be spawned.
+     * 
+     * @param entity The entity
+     * @param includePosition If true position data will be kept
+     */
+    public void setSpawnedEntity(Entity entity, boolean includePosition);
 
     /**
      * Set the spawner creature type.
@@ -85,4 +101,88 @@ public interface CreatureSpawner extends BlockState {
      * @param delay The delay.
      */
     public void setDelay(int delay);
+
+    /**
+     * Get the spawner's minimum delay.
+     *
+     * @return The delay.
+     */
+    public int getMinDelay();
+
+    /**
+     * Set the spawner's minimum delay.
+     *
+     * @param delay The delay.
+     */
+    public void setMinDelay(int delay);
+
+    /**
+     * Get the spawner's maximum delay.
+     *
+     * @return The delay.
+     */
+    public int getMaxDelay();
+
+    /**
+     * Set the spawner's maximum delay.
+     *
+     * @param delay The delay.
+     */
+    public void setMaxDelay(int delay);
+
+    /**
+     * Gets the number of entities that the spawner will attempt to spawn.
+     * 
+     * @return The number.
+     */
+    public int getCount();
+
+    /**
+     * Sets the number of entities that the spawner will attempt to spawn.
+     * 
+     * @param count The number.
+     */
+    public void setCount(int count);
+
+    /**
+     * Gets the maximum distance from the spawner that entities will be spawned.
+     * 
+     * @return The distance.
+     */
+    public int getRange();
+
+    /**
+     * Sets the maximum distance from the spawner that entities will be spawned.
+     * 
+     * @param range The distance.
+     */
+    public void setRange(int range);
+
+    /**
+     * Gets the number of other entities that need to be within the spawner's range to prevent spawning,
+     * 
+     * @return The number.
+     */
+    public int getMaxNearbyEntities();
+
+    /**
+     * Sets the number of other entities that need to be within the spawner's range to prevent spawning.
+     * 
+     * @param maxNearbyEntities The number.
+     */
+    public void setMaxNearbyEntities(int maxNearbyEntities);
+
+    /**
+     * Gets the range that a player must be within to allow spawning.
+     * 
+     * @return The range.
+     */
+    public int getRequiredPlayerRange();
+
+    /**
+     * Sets the range that a player must be within to allow spawning.
+     * 
+     * @param requiredPlayerRange The range.
+     */
+    public void setRequiredPlayerRange(int requiredPlayerRange);
 }
