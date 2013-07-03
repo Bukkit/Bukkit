93386b7105f94702b4471942b6f822bdd97621e7
rockon999 <junkmail9969@gmail.com>
2013-07-03 18:46:46 -0500

    Added Horse Types To Horse.java.
    This doesn't add the 35 different combinations of NORMAL Horses, but it would allow us to set the type of the horse... Source For TypeS: http://www.minecraftwiki.net/wiki/Item_id#Horse_Variants

--- src/main/java/org/bukkit/entity/Horse.java
+++ src/main/java/org/bukkit/entity/Horse.java
@@ -3,4 +3,44 @@ package org.bukkit.entity;
 /**
  * Represents a Horse.
  */
-public interface Horse extends Animals, Vehicle {}
+public interface Horse extends Animals, Vehicle {
+
+    public Type getHorseType();
+
+
+    public void setHorseType(HorseType type);
+
+    public enum HorseType {
+        NORMAL(0),
+        DONKEY(1),
+        MULE(2),
+        UNDEAD(3);
+        SKELETAL(4);
+
+        private static final HorseType[] types = new HorseType[HorseType.values().length];
+        private final int id;
+
+        static {
+            for (HorseType type : values()) {
+                types[type.getId()] = type;
+            }
+        }
+
+        private HorseType(int id) {
+            this.id = id;
+        }
+
+     
+        public int getId() {
+            return id;
+        }
+
+      
+        public static HorseType getType(int id) {
+            return (id >= types.length) ? null : types[id];
+        }
+    }
+  
+  
+  
+  }
