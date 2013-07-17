3abca56652176d7130f2dcd1ba570c6d4c7f73c1
Travis Ralston <travpc@gmail.com>
2013-07-16 23:07:13 -0600

    Fix complaints

--- src/main/java/org/bukkit/event/entity/EnderSignalLaunchEvent.java
+++ src/main/java/org/bukkit/event/entity/EnderSignalLaunchEvent.java
@@ -7,25 +7,26 @@ import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 
 /**
- * Thrown when an ender signal (eye of ender) is launched.<br>
+ * Thrown when an ender signal (eye of ender) is launched.
+ * <br>
  * This currently can only be initiated by a player.
  */
 public class EnderSignalLaunchEvent extends EntityEvent implements Cancellable{
     private static final HandlerList handlers = new HandlerList();
-    private HumanEntity shooter;
+    private HumanEntity launcher;
     private boolean cancel = false;
     
     public EnderSignalLaunchEvent(Entity what, HumanEntity shooter){
         super(what);
-        this.shooter = shooter;
+        this.launcher = shooter;
     }
     
     /**
-     * Gets who launched the ender signal
-     * @return the shooter
+     * Gets who launched the ender signal.
+     * @return the launcher of the ender signal
      */
-    public HumanEntity getShooter(){
-        return shooter;
+    public HumanEntity getLauncher(){
+        return launcher;
     }
     
     @Override
