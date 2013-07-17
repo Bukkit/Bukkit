18efe66ba94cb32406b6c93d70dc9c68b49e0d95
T00thpick1 <t00thpick1dirko@gmail.com>
2013-07-17 00:30:29 -0400

    Wither API. Adds BUKKIT-4571

--- src/main/java/org/bukkit/entity/Wither.java
+++ src/main/java/org/bukkit/entity/Wither.java
@@ -1,7 +1,46 @@
 package org.bukkit.entity;
 
+import org.bukkit.Location;
+
 /**
  * Represents a Wither boss
  */
 public interface Wither extends Monster {
+    /**
+     * Gets the current target of a wither head
+     *
+     * @param head which head to check the target of
+     * @return the current target of a wither head, or null if none
+     */
+    public LivingEntity getTarget(WitherHead head);
+    /**
+     * Sets the current target of a wither head
+     *
+     * @param head which head to check the target of
+     * @param entity the entity to set as the target
+     */
+    public void setTarget(WitherHead head, LivingEntity entity);
+    /**
+     * Fires a wither skull from a wither head
+     *
+     * @param head which head to fire from
+     * @param entity the entity to fire at
+     */
+    public void shoot(WitherHead head, LivingEntity entity);
+    /**
+     * Fires a wither skull from a wither head
+     *
+     * @param head which head to fire from
+     * @param location the location to fire at
+     */
+    public void shoot(WitherHead head, Location location);
+    
+    /**
+     * An enum to specify which head of the wither
+     */
+    public enum WitherHead {
+        LEFT,
+        CENTER,
+        RIGHT,
+    }
 }
---
+++ src/main/java/org/bukkit/event/entity/WitherHeadTargetEvent.java
@@ -0,0 +1,63 @@
+package org.bukkit.event.entity;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Wither.WitherHead;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+
+public class WitherHeadTargetEvent extends EntityEvent implements Cancellable {
+    private static final HandlerList handlers = new HandlerList();
+    private boolean cancel = false;
+    private WitherHead head;
+    private LivingEntity target;
+
+    public WitherHeadTargetEvent(Entity entity, LivingEntity target, WitherHead head) {
+        super(entity);
+        this.head = head;
+        this.target = target;
+    }
+
+    public WitherHead getWitherHead() {
+        return head;
+    }
+
+    public boolean isCancelled() {
+        return cancel;
+    }
+
+    public void setCancelled(boolean cancel) {
+        this.cancel = cancel;
+    }
+
+    /**
+     * Get the entity that this is targeting.
+     *
+     * @return The entity
+     */
+    public LivingEntity getTarget() {
+        return target;
+    }
+
+    /**
+     * Set the entity that you want the mob to target instead.
+     * It is possible to be null, null will cause the entity to be
+     * target-less.
+     * <p>
+     * Setting this to null is essentially the same as cancelling the event
+     *
+     * @param target The entity to target
+     */
+    public void setTarget(LivingEntity target) {
+        this.target = target;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
