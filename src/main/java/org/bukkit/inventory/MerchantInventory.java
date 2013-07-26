8ae7b629bbdcaf876ae0fdfffdcae2d410437235
modwizcode <starbuck@modwiz.com>
2013-07-26 12:41:46 -0400

    Updated Villager Merchant API to fit with vanilla concepts better.

--- src/main/java/org/bukkit/inventory/MerchantInventory.java
+++ src/main/java/org/bukkit/inventory/MerchantInventory.java
@@ -1,27 +1,20 @@
 package org.bukkit.inventory;
 
+import java.util.List;
+
 public interface MerchantInventory extends Inventory {
     /**
-     * Returns the number of pages currently exposed to the player.
+     * Returns the number of offers currently available.
      * 
-     * @return The number of trade pages exposed to the player.
+     * @return offers available
      */
-    public int getExposedPageCount();
+    public int getOfferCount();
     
     /**
-     * Returns if the given page is locked.
-     *
-     * @param pageNumber The page number to check.
-     * @return True if the page is locked, false if it is not.
-     */
-    public boolean isPageLocked(int pageNumber);
-    
-    /**
-     * Returns the specified trading page. A value of null is given for an
-     * invalid page number.
+     * Returns a copy of the current offer list. Modifications have to
+     * be added by calling the correct method.
      * 
-     * @param pageNumber The trading page to return
-     * @return A {@link VillagerTrade} representing the trading page.
+     * @return list of all offers
      */
-    public VillagerTrade getTrade(int pageNumber);
+    public List<VillagerTrade> getOffers();
 }
