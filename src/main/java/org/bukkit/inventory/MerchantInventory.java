8604b6ce3f5d55e7206afc291fc3e9f7631cbe7d
modwizcode <starbuck@modwiz.com>
2013-07-26 17:53:39 -0400

    Added interface to clear trades list and also added parameterless getMatchingOffer for the inventory to automatically take the items already there.

--- src/main/java/org/bukkit/inventory/MerchantInventory.java
+++ src/main/java/org/bukkit/inventory/MerchantInventory.java
@@ -4,8 +4,16 @@ public interface MerchantInventory extends Inventory {
     /**
      * Returns a copy of the MerchantOffers object that performs all operations on
      * the trades provided.
-     * 
+     *
      * @return The merchant offers
      */
     public MerchantOffers getOffers();
+
+    /**
+     * Returns a matching offer for the two input stacks, if none are found
+     * null is returned.
+     *
+     * @return A trade that matches the current inventory or null if no match is found.
+     */
+    public VillagerTrade getMatchingOffer();
 }
