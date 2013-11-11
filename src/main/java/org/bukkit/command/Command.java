0fa9e1baf9a638fefb7ad9633f390537c5f0ed6f
Chrisdamonster <thecodermc@gmail.com>
2013-11-10 20:13:45 -0500

    Update Command.java
    I made the no permission message sound not as "mean" as it did before.

--- src/main/java/org/bukkit/command/Command.java
+++ src/main/java/org/bukkit/command/Command.java
@@ -139,7 +139,7 @@ public abstract class Command {
         }
 
         if (permissionMessage == null) {
-            target.sendMessage(ChatColor.RED + "I'm sorry, but you do not have permission to perform this command. Please contact the server administrators if you believe that this is in error.");
+            target.sendMessage(ChatColor.RED + "I'm sorry, but you don't have the right permissions to peform this command. If you believe this is an error, please contact a server administrator");
         } else if (permissionMessage.length() != 0) {
             for (String line : permissionMessage.replace("<permission>", permission).split("\n")) {
                 target.sendMessage(line);
