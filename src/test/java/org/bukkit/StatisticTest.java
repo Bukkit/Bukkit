2a766a6afeb3ff1cd01c022c0a00a842d2392379
imgithuber2 <imgithuber2@gmail.com>
2013-11-16 16:54:16 -0800

    my file

--- src/test/java/org/bukkit/StatisticTest.java
+++ src/test/java/org/bukkit/StatisticTest.java
@@ -1,15 +1,8 @@
-package org.bukkit;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
-import org.junit.Test;
-
-public class StatisticTest {
-    @Test
-    public void getById() {
-        for (Statistic statistic : Statistic.values()) {
-            assertThat(Statistic.getById(statistic.getId()), is(statistic));
-        }
-    }
+public class status extends statustest{
+        bukkit=false;
+        wolveereneess=false;
+        github=true;
+        imgithuber2=true;
+        wolveismeen=true;
+        bukkitismean=true;
 }
