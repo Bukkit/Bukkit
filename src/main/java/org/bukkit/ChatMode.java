7ecb07a3df0df4762ed9bbc8a05ddd419ca0a81a
Fabian Fassbender <fabian.fassbender42@googlemail.com>
2013-12-08 13:43:10 +0100

    No magic values inside Bukkit API

--- src/main/java/org/bukkit/ChatMode.java
+++ src/main/java/org/bukkit/ChatMode.java
@@ -1,36 +1,7 @@
 package org.bukkit;
 
 public enum ChatMode {
-    FULL(0),
-    COMMAND_ONLY(1),
-    HIDDEN(2);
-
-    private int intMode;
-
-    private ChatMode(int mode) {
-        this.intMode = mode;
-    }
-
-    /**
-     * Get the int representing the ChatMode
-     *
-     * @return int representation of the ChatMode
-     */
-    public int getIntMode() {
-        return intMode;
-    }
-
-    /**
-     * Get the correct ChatMode Enum based on the mode
-     *
-     * @param mode Mode as integer
-     * @return Correct ChatMode Enum or null if none was found
-     */
-    public static ChatMode getByIntMode(int mode) {
-        for (ChatMode m : values()) {
-            if (m.getIntMode() == mode) return m;
-        }
-
-        return null;
-    }
+    FULL,
+    COMMAND_ONLY,
+    HIDDEN
 }
