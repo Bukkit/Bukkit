e66f09f923b19a5cafeab64363f0a48c09333782
Likaos <romain.rosique@gmail.com>
2013-12-08 13:52:45 +0100

    add setKnockbackStrengh for arrows

--- src/main/java/org/bukkit/entity/Arrow.java
+++ src/main/java/org/bukkit/entity/Arrow.java
@@ -3,4 +3,12 @@ package org.bukkit.entity;
 /**
  * Represents an arrow.
  */
-public interface Arrow extends Projectile {}
+public interface Arrow extends Projectile {
+	
+    /**
+   * Set the knockbackStrenght for an arrow. Must be nonnegative.
+   *
+   * @param knockbackStrength the punch level effect.
+   */
+  public void setKnockbackStrength(int knockbackStrength);	
+}
