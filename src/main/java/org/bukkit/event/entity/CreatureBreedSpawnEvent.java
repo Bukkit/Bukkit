12a2272dacbe5890d89b969035ddf7f17cc2acad
ltcyx <leethus@gmail.com>
2014-01-30 03:26:18 -0200

    * New event for when a creature is added to the world because of breeding. Adds BUKKIT-5357

---
+++ src/main/java/org/bukkit/event/entity/CreatureBreedSpawnEvent.java
@@ -0,0 +1,28 @@
+package org.bukkit.event.entity;
+
+import org.bukkit.entity.LivingEntity;
+
+/**
+ * Called when a creature spawns because of breeding.
+ */
+public class CreatureBreedSpawnEvent extends CreatureSpawnEvent {
+	private LivingEntity parent1;
+	private LivingEntity parent2;
+	
+	public CreatureBreedSpawnEvent(LivingEntity spawnee, LivingEntity parent1, LivingEntity parent2) {
+		super(spawnee, SpawnReason.BREEDING);
+		// TODO Auto-generated constructor stub
+		this.parent1 = parent1;
+		this.parent2 = parent2;
+	}
+
+	public LivingEntity getParent1() {
+		return parent1;
+	}
+
+	public LivingEntity getParent2() {
+		return parent2;
+	}
+	
+	
+}
