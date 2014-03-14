9a3bacb735cdc64d53e76252e32f26061dbbc935
Kodek <Kodekgames@gmail.com>
2014-03-14 14:16:02 +0100

    Changed flowerpot contents as ItemStack to MaterialData

--- src/main/java/org/bukkit/block/FlowerPot.java
+++ src/main/java/org/bukkit/block/FlowerPot.java
@@ -1,6 +1,6 @@
 package org.bukkit.block;
 
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.MaterialData;
 
 /**
  * Represents a flower pot.
@@ -10,15 +10,15 @@ public interface FlowerPot extends BlockState {
     /**
      * Get the material in the flower pot
      * 
-     * @return item ItemStack for the item currently in the flower pot (material is AIR if it is empty).
+     * @return item MaterialData for the item currently in the flower pot (material is AIR if it is empty).
      */
-    ItemStack getContents();
+    MaterialData getContents();
 
     /**
      * Set the contents of the flower pot
      * 
-     * @param itemStack ItemStack of the item to put in the flower pot or null/material as AIR to empty it.
+     * @param materialData MaterialData of the item to put in the flower pot or null/material as AIR to empty it.
      */
-    void setContents(ItemStack itemStack);
+    void setContents(MaterialData materialData);
 
 }
