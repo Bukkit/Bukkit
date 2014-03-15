3e8992d6f438ee2a85ab76ec5fee1db2a4ef8a80
Kenzie Togami <ket1999@gmail.com>
2014-03-15 10:09:22 -0700

    Add EntitySpawnEvent. Adds Bukkit-1559

--- src/main/java/org/bukkit/event/entity/EntitySpawnEvent.java
+++ src/main/java/org/bukkit/event/entity/EntitySpawnEvent.java
@@ -1,16 +1,30 @@
 package org.bukkit.event.entity;
 
 import org.bukkit.Location;
-import org.bukkit.entity.CreatureType;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 
 /**
- * Called when a entity is spawned into a world.
+ * Called when a entity is spawned into a world. This is not called for some
+ * entities, see table below for the right events for those.
  * <p>
  * If a Entity Spawn event is cancelled, the entity will not spawn.
+ * </p>
+ * <p>
+ * Entity spawns not handled by this event are {@link LivingEntity},
+ * {@link Item}, and {@link Projectile} spawns. They can be tracked with the
+ * existing events {@link CreatureSpawnEvent}, {@link ItemSpawnEvent}, and
+ * {@link ProjectileLaunchEvent}, respectively.
+ * </p>
+ * 
+ * @see CreatureSpawnEvent
+ * @see ItemSpawnEvent
+ * @see ProjectileLaunchEvent
  */
 public class EntitySpawnEvent extends EntityEvent implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
@@ -31,13 +45,13 @@ public class EntitySpawnEvent extends EntityEvent implements Cancellable {
     }
 
     @Override
-    public LivingEntity getEntity() {
-        return (LivingEntity) entity;
+    public Entity getEntity() {
+        return entity;
     }
 
     /**
      * Gets the location at which the entity is spawning.
-     *
+     * 
      * @return The location at which the entity is spawning
      */
     public Location getLocation() {
@@ -46,9 +60,9 @@ public class EntitySpawnEvent extends EntityEvent implements Cancellable {
 
     /**
      * Gets the reason for why the entity is being spawned.
-     *
+     * 
      * @return A SpawnReason value detailing the reason for the entity being
-     *     spawned
+     *         spawned
      */
     public SpawnReason getSpawnReason() {
         return spawnReason;
@@ -61,23 +75,5 @@ public class EntitySpawnEvent extends EntityEvent implements Cancellable {
 
     public static HandlerList getHandlerList() {
         return handlers;
-    }
-
-    /**
-     * An enum to specify the type of spawning
-     */
-    public enum SpawnReason {
-    	/**
-    	 * When an entity is spawned via dispenser
-    	 */
-    	DISPENSER,
-        /**
-         * When an entity is spawned by plugins
-         */
-        CUSTOM,
-        /**
-         * When an entity is missing a SpawnReason
-         */
-        DEFAULT
     }
 }
