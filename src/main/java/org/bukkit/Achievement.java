38015e2ae86381fd6f404b38c863d0e0acd07a49
unon1100 <theuniongaming@gmail.com>
2014-04-06 11:14:49 -0500

    Achievements with new Minecraft IDs
    Minecraft now has IDs for each achievement and they can be used in things such as tellraw. Might as well have this available

--- src/main/java/org/bukkit/Achievement.java
+++ src/main/java/org/bukkit/Achievement.java
@@ -4,49 +4,52 @@ package org.bukkit;
  * Represents an achievement, which may be given to players.
  */
 public enum Achievement {
-    OPEN_INVENTORY,
-    MINE_WOOD (OPEN_INVENTORY),
-    BUILD_WORKBENCH (MINE_WOOD),
-    BUILD_PICKAXE (BUILD_WORKBENCH),
-    BUILD_FURNACE (BUILD_PICKAXE),
-    ACQUIRE_IRON (BUILD_FURNACE),
-    BUILD_HOE (BUILD_WORKBENCH),
-    MAKE_BREAD (BUILD_HOE),
-    BAKE_CAKE (BUILD_HOE),
-    BUILD_BETTER_PICKAXE (BUILD_PICKAXE),
-    COOK_FISH (BUILD_FURNACE),
-    ON_A_RAIL (ACQUIRE_IRON),
-    BUILD_SWORD (BUILD_WORKBENCH),
-    KILL_ENEMY (BUILD_SWORD),
-    KILL_COW (BUILD_SWORD),
-    FLY_PIG (KILL_COW),
-    SNIPE_SKELETON (KILL_ENEMY),
-    GET_DIAMONDS (ACQUIRE_IRON),
-    NETHER_PORTAL (GET_DIAMONDS),
-    GHAST_RETURN (NETHER_PORTAL),
-    GET_BLAZE_ROD (NETHER_PORTAL),
-    BREW_POTION (GET_BLAZE_ROD),
-    END_PORTAL (GET_BLAZE_ROD),
-    THE_END (END_PORTAL),
-    ENCHANTMENTS (GET_DIAMONDS),
-    OVERKILL (ENCHANTMENTS),
-    BOOKCASE (ENCHANTMENTS),
-    EXPLORE_ALL_BIOMES (END_PORTAL),
-    SPAWN_WITHER (THE_END),
-    KILL_WITHER (SPAWN_WITHER),
-    FULL_BEACON (KILL_WITHER),
-    BREED_COW (KILL_COW),
-    DIAMONDS_TO_YOU (GET_DIAMONDS),
+    OPEN_INVENTORY("openInventory"),
+    MINE_WOOD ("mineWood", OPEN_INVENTORY),
+    BUILD_WORKBENCH ("buildWorkBench", MINE_WOOD),
+    BUILD_PICKAXE ("buildPickaxe", BUILD_WORKBENCH),
+    BUILD_FURNACE ("buildFurnace", BUILD_PICKAXE),
+    ACQUIRE_IRON ("acquireIron", BUILD_FURNACE),
+    BUILD_HOE ("buildHoe", BUILD_WORKBENCH),
+    MAKE_BREAD ("makeBread", BUILD_HOE),
+    BAKE_CAKE ("bakeCake", BUILD_HOE),
+    BUILD_BETTER_PICKAXE ("buildBetterPickaxe", BUILD_PICKAXE),
+    COOK_FISH ("cookFish", BUILD_FURNACE),
+    ON_A_RAIL ("onARail", ACQUIRE_IRON),
+    BUILD_SWORD ("buildSword", BUILD_WORKBENCH),
+    KILL_ENEMY ("killEnemy", BUILD_SWORD),
+    KILL_COW ("killCow", BUILD_SWORD),
+    FLY_PIG ("flyPig", KILL_COW),
+    SNIPE_SKELETON ("snipeSkeleton", KILL_ENEMY),
+    GET_DIAMONDS ("diamonds", ACQUIRE_IRON),
+    NETHER_PORTAL ("portal", GET_DIAMONDS),
+    GHAST_RETURN ("ghast", NETHER_PORTAL),
+    GET_BLAZE_ROD ("blazeRod", NETHER_PORTAL),
+    BREW_POTION ("potion", GET_BLAZE_ROD),
+    END_PORTAL ("theEnd", GET_BLAZE_ROD),
+    THE_END ("theEnd2", END_PORTAL),
+    ENCHANTMENTS ("enchantments", GET_DIAMONDS),
+    OVERKILL ("overkill", ENCHANTMENTS),
+    BOOKCASE ("bookcase", ENCHANTMENTS),
+    EXPLORE_ALL_BIOMES ("exploreAllBiomes", END_PORTAL),
+    SPAWN_WITHER ("spawnWither", THE_END),
+    KILL_WITHER ("killWither", SPAWN_WITHER),
+    FULL_BEACON ("fullBeacon", KILL_WITHER),
+    BREED_COW ("breedCow", KILL_COW),
+    DIAMONDS_TO_YOU ("diamondsToYou", GET_DIAMONDS),
     ;
 
     private final Achievement parent;
+    private final String id;
 
-    private Achievement() {
+    private Achievement(String id) {
         parent = null;
+        this.id = id;
     }
 
-    private Achievement(Achievement parent) {
+    private Achievement(String id, Achievement parent) {
         this.parent = parent;
+        this.id = id;
     }
 
     /**
@@ -66,4 +69,14 @@ public enum Achievement {
     public Achievement getParent() {
         return parent;
     }
+    
+    /**
+     * Returns the Minecraft id string of this achievement
+     * 
+     * @return the Minecraft id of the achievement
+     */
+    public String getID(){
+    	return "minecraft:" + id;
+    }
 }
+
