a53497b67e3449951aa60d9b1802af7e533852c6
Nathan Wolf <nathan@elmakers.com>
2014-05-21 21:41:25 -0700

    Clean up PersistentMetadataValue
    Extend MetadataValueAdapter directly for performance, and remove all
    the variant constructor types. They were meant to serve as a
    compile-time suggestion for what types are ok, but in practice it feels
    cluttered.

--- src/main/java/org/bukkit/metadata/PersistentMetadataValue.java
+++ src/main/java/org/bukkit/metadata/PersistentMetadataValue.java
@@ -8,137 +8,32 @@ import java.util.Map;
 
 /**
  * A PersistentMetadataValue is a special case metadata item that may
- * only contain ConfigurationSerializeable Objects, Lists, Maps or
+ * only contain ConfigurationSerializable Objects, Lists, Maps or
  * basic Object types.
  * <p>
- * This class extends FixedMetadataValue to do the work, and is
- * primarily here to serve as a reminder that ItemMeta holds
- * a slightly different form of Metadata.
+ * This class is primarily here to serve as a reminder that
+ * a persistent data store must hold serializable data.
  */
-public class PersistentMetadataValue extends FixedMetadataValue {
+public class PersistentMetadataValue extends MetadataValueAdapter implements MetadataValue {
     /**
-     * Initializes a PersistentMetadataValue with a
-     * ConfigurationSerializable Object
-     *
-     * @param owningPlugin the {@link org.bukkit.plugin.Plugin} that created this metadata value
-     * @param value the value assigned to this metadata value
+     * Store the internal value that is represented by this value.
+     * This is expected to be one of a set of a valid serializable types.
      */
-    public PersistentMetadataValue(Plugin owningPlugin, final ConfigurationSerializable value) {
-        super(owningPlugin, value);
-    }
-
-    /**
-     * Initializes a PersistentMetadataValue with a String value.
-     *
-     * @param owningPlugin the {@link org.bukkit.plugin.Plugin} that created this metadata value
-     * @param value the value assigned to this metadata value
-     */
-    public PersistentMetadataValue(Plugin owningPlugin, final String value) {
-        super(owningPlugin, value);
-    }
-
-    /**
-     * Initializes a PersistentMetadataValue with an Integer value.
-     *
-     * @param owningPlugin the {@link org.bukkit.plugin.Plugin} that created this metadata value
-     * @param value the value assigned to this metadata value
-     */
-    public PersistentMetadataValue(Plugin owningPlugin, final Integer value) {
-        super(owningPlugin, value);
-    }
-
-    /**
-     * Initializes a PersistentMetadataValue with a Boolean value.
-     *
-     * @param owningPlugin the {@link org.bukkit.plugin.Plugin} that created this metadata value
-     * @param value the value assigned to this metadata value
-     */
-    public PersistentMetadataValue(Plugin owningPlugin, final Boolean value) {
-        super(owningPlugin, value);
-    }
-
-    /**
-     * Initializes a PersistentMetadataValue with a Long value.
-     *
-     * @param owningPlugin the {@link org.bukkit.plugin.Plugin} that created this metadata value
-     * @param value the value assigned to this metadata value
-     */
-    public PersistentMetadataValue(Plugin owningPlugin, final Long value) {
-        super(owningPlugin, value);
-    }
-
-    /**
-     * Initializes a PersistentMetadataValue with a Double value.
-     *
-     * @param owningPlugin the {@link org.bukkit.plugin.Plugin} that created this metadata value
-     * @param value the value assigned to this metadata value
-     */
-    public PersistentMetadataValue(Plugin owningPlugin, final Double value) {
-        super(owningPlugin, value);
-    }
-
-    /**
-     * Initializes a PersistentMetadataValue with a Short value.
-     *
-     * @param owningPlugin the {@link org.bukkit.plugin.Plugin} that created this metadata value
-     * @param value the value assigned to this metadata value
-     */
-    public PersistentMetadataValue(Plugin owningPlugin, final Short value) {
-        super(owningPlugin, value);
-    }
-
-    /**
-     * Initializes a PersistentMetadataValue with a Float value.
-     *
-     * @param owningPlugin the {@link org.bukkit.plugin.Plugin} that created this metadata value
-     * @param value the value assigned to this metadata value
-     */
-    public PersistentMetadataValue(Plugin owningPlugin, final Float value) {
-        super(owningPlugin, value);
-    }
-
-    /**
-     * Initializes a PersistentMetadataValue with a List value.
-     * <p>
-     * Note that the contents of the List must also be persistable,
-     * otherwise an IllegalArgumentException may be generated when
-     * the metadata store persists.
-     *
-     * @param owningPlugin the {@link org.bukkit.plugin.Plugin} that created this metadata value
-     * @param value the value assigned to this metadata value
-     */
-    public PersistentMetadataValue(Plugin owningPlugin, final List<?> value) {
-        super(owningPlugin, value);
-    }
-
-    /**
-     * Initializes a PersistentMetadataValue with a Map value.
-     * <p>
-     * Note that the keys of the Map must be Strings, and the
-     * contents of the Map must be persistable,
-     * otherwise an IllegalArgumentException may be generated when
-     * the metadata store persists.
-     *
-     * @param owningPlugin the {@link org.bukkit.plugin.Plugin} that created this metadata value
-     * @param value the value assigned to this metadata value
-     */
-    public PersistentMetadataValue(Plugin owningPlugin, final Map<String, ?> value) {
-        super(owningPlugin, value);
-    }
+    private final Object internalValue;
 
     /**
      * Initializes a PersistentMetadataValue with an Object value.
      * <p>
      * The Object must be one of the valid persistable object types.
-     * The other Constructor forms are preferred, this is here as a
-     * convenience method.
+     * Otherwise, an IllegalArgumentException will be thrown.
      *
      * @throws java.lang.IllegalArgumentException on non-persistable input
      * @param owningPlugin the {@link org.bukkit.plugin.Plugin} that created this metadata value
      * @param value the value assigned to this metadata value
      */
     public PersistentMetadataValue(Plugin owningPlugin, final Object value) {
-        super(owningPlugin, value);
+        super(owningPlugin);
+        this.internalValue = value;
 
         if (!(
               value instanceof ConfigurationSerializable
@@ -154,5 +49,15 @@ public class PersistentMetadataValue extends FixedMetadataValue {
         )) {
             throw new IllegalArgumentException("Invalid Object type for PersistentMetadataValue: " + value.getClass());
         }
+    }
+
+    @Override
+    public Object value() {
+        return internalValue;
+    }
+
+    @Override
+    public void invalidate() {
+
     }
 }
