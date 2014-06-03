06c75bea8b7b69f81efa8dbd842708c84ca462bd
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2014-06-02 22:06:21 -0700

    Add an EntityDespawnEvent. Fixes BUKKIT-5645
    There was not any event fired for when an entity despawns for reasons
    other than death, EG when a non-persistent entity is removed because no
    player is nearby (RemoveWhenFarAway). This commit adds a simple
    entity-despawn-event to fill this void, to be used with a matching CB
    commit that will fire this event.

---
+++ src/main/java/org/bukkit/event/entity/EntityDespawnEvent.java
@@ -0,0 +1,24 @@
+package org.bukkit.event.entity;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.event.HandlerList;
+
+/**
+ * Thrown whenever an Entity is removed from the world
+ */
+public class EntityDespawnEvent extends EntityEvent {
+    private static final HandlerList handlers = new HandlerList();
+
+    public EntityDespawnEvent(final Entity entity) {
+        super(entity);
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
