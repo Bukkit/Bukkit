2911773ddc9e869402bd7f6b7f7f7244fb43548b
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2014-07-11 15:17:58 -0700

    Add a way to induce mob breeding, adds BUKKIT-5687
    Adds a system to induce mob breeding through the Bukkit API.

--- src/main/java/org/bukkit/entity/Animals.java
+++ src/main/java/org/bukkit/entity/Animals.java
@@ -1,6 +1,32 @@
 package org.bukkit.entity;
 
+import org.bukkit.entity.Player;
+
 /**
  * Represents an Animal.
  */
-public interface Animals extends Ageable {}
+public interface Animals extends Ageable {
+
+    /**
+     * Determines if this animal is currently breeding.
+     *
+     * @return true if it is breeding.
+     */
+    public boolean isBreeding();
+
+    /**
+     * Set whether this animal is currently trying to breed.
+     *
+     * @param breeding whether the animal should try to breed.
+	 * @param player the player that induced the breeding.
+     */
+    public void setBreeding(boolean breeding, Player player);
+
+    /**
+     * Set whether this animal is currently trying to breed.
+     *
+     * @param breeding whether the animal should try to breed.
+     */
+    public void setBreeding(boolean breeding);
+
+}
