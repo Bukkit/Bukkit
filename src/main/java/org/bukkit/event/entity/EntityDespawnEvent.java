47521e0a9898c4acfd377a3f5e83225efbfcddab
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2014-07-20 17:00:06 -0700

    Fix formatting, some info

--- src/main/java/org/bukkit/event/entity/EntityDespawnEvent.java
+++ src/main/java/org/bukkit/event/entity/EntityDespawnEvent.java
@@ -7,42 +7,10 @@ import org.bukkit.event.HandlerList;
  * Called whenever an Entity is removed from the world.
  */
 public class EntityDespawnEvent extends EntityEvent {
-    private static final HandlerList handlers = new HandlerList();
-    private final DespawnReason despawnReason;
-
-    public EntityDespawnEvent(final Entity entity) {
-        super(entity);
-        despawnReason = DespawnReason.DEFAULT;
-    }
-
-    public EntityDespawnEvent(final Entity entity, DespawnReason reason) {
-        super(entity);
-        despawnReason = reason;
-    }
-
-    /**
-     * Gets the reason for why the creature is being despawned.
-     *
-     * @return A DespawnReason value detailing the reason for the entity being despawned
-     */
-    public DespawnReason getDespawnReason() {
-        return despawnReason;
-    }
-
-    @Override
-    public HandlerList getHandlers() {
-        return handlers;
-    }
-
-    public static HandlerList getHandlerList() {
-        return handlers;
-    }
-
     /**
      * An enum to specify the type of despawning
      */
     public enum DespawnReason {
-
         /**
          * When an entity despawns due to a chunk unload
          */
@@ -60,5 +28,36 @@ public class EntityDespawnEvent extends EntityEvent {
          * When an entity is missing a DespawnReason
          */
         DEFAULT
+    }
+    
+    private static final HandlerList handlers = new HandlerList();
+    private final DespawnReason despawnReason;
+
+    public EntityDespawnEvent(final Entity entity) {
+        super(entity);
+        despawnReason = DespawnReason.DEFAULT;
+    }
+
+    public EntityDespawnEvent(final Entity entity, DespawnReason reason) {
+        super(entity);
+        despawnReason = reason;
+    }
+
+    /**
+     * Gets the reason for why the entity is being despawned.
+     *
+     * @return The {@link DespawnReason} detailing the reason for the entity being despawned
+     */
+    public DespawnReason getDespawnReason() {
+        return despawnReason;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
     }
 }
