fc31dd0ab84f68fb323b5477eaa6dbc054099b5e
xa112 <felixalex1999@gmail.com>
2014-07-26 00:27:19 -0700

    Fixed isSolid() in Material
    isSolid() returns nonsolid blocks as being solid and doesn't include
    FLOWER_POT as being solid.

--- src/main/java/org/bukkit/Material.java
+++ src/main/java/org/bukkit/Material.java
@@ -698,13 +698,9 @@ public enum Material {
             case SOIL:
             case FURNACE:
             case BURNING_FURNACE:
-            case SIGN_POST:
             case WOODEN_DOOR:
             case COBBLESTONE_STAIRS:
-            case WALL_SIGN:
-            case STONE_PLATE:
             case IRON_DOOR_BLOCK:
-            case WOOD_PLATE:
             case REDSTONE_ORE:
             case GLOWING_REDSTONE_ORE:
             case ICE:
@@ -758,8 +754,6 @@ public enum Material {
             case COBBLE_WALL:
             case ANVIL:
             case TRAPPED_CHEST:
-            case GOLD_PLATE:
-            case IRON_PLATE:
             case DAYLIGHT_DETECTOR:
             case REDSTONE_BLOCK:
             case QUARTZ_ORE:
@@ -777,6 +771,7 @@ public enum Material {
             case ACACIA_STAIRS:
             case DARK_OAK_STAIRS:
             case PACKED_ICE:
+            case FLOWER_POT:
                 return true;
             default:
                 return false;
