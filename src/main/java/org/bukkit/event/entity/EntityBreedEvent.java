9e1b19ffbe5310a35d54b440408bc11e395d35a5
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2014-08-05 14:16:05 -0700

    Remove breeder from event

--- src/main/java/org/bukkit/event/entity/EntityBreedEvent.java
+++ src/main/java/org/bukkit/event/entity/EntityBreedEvent.java
@@ -13,14 +13,12 @@ public class EntityBreedEvent extends EntityEvent implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
     private final Animals entityTwo;
     private final Ageable baby;
-    private Player breeder;
     private boolean cancelled;
     private int xp;
 
-    public EntityBreedEvent(final Animals entity, final Animals entityTwo, final Player breeder, final Ageable baby, final int xp) {
+    public EntityBreedEvent(final Animals entity, final Animals entityTwo, final Ageable baby, final int xp) {
         super(entity);
         this.entityTwo = entityTwo;
-        this.breeder = breeder;
         this.baby = baby;
         this.xp = xp;
     }
@@ -56,29 +54,6 @@ public class EntityBreedEvent extends EntityEvent implements Cancellable {
     */
     public Ageable getBaby() {
         return baby;
-    }
-
-    /**
-    * Gets the player that started the breeding, if any.
-    * If breeding was induced by plugin, may return null.
-    *
-    * @returns the breeder player, or null if none.
-    */
-    public Player getBreeder() {
-        return breeder;
-    }
-
-    /**
-    * Sets the player that is considered to have started the breeding,
-    * if any.
-    * The player set will receive potential rewards for breeding,
-    * (EG: an achievement).
-    * Set the player null to give nobody the reward.
-    *
-    * @param player the new breeder player, or null for none.
-    */
-    public void setBreeder(Player player) {
-        breeder = player;
     }
 
     public boolean isCancelled() {
