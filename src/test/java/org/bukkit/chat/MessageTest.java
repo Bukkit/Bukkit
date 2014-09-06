1d1875d258270969dba1efffd05483fbcdc47b8a
ST-DDT <ST-DDT@gmx.de>
2014-09-06 16:20:03 +0200

    Add tests for clone methods, fixed whitespace issues.

--- src/test/java/org/bukkit/chat/MessageTest.java
+++ src/test/java/org/bukkit/chat/MessageTest.java
@@ -11,10 +11,19 @@ import org.junit.Test;
 public class MessageTest {
 
     @Test
+    public void testClone() {
+        final Message message = Message.of("Bukkit is awesome!")
+                .append(new ItemStack(Material.COOKIE, 42), ChatColor.RED + "Here your awesome cookie!")
+                .append(Achievement.BAKE_CAKE, ChatColor.UNDERLINE + "Here your awesome cake!")
+                .append(Click.ofOpenUrl("http://www.bukkit.org"), "Click here for more information.");
+        assertEquals(message, message.clone());
+    }
+
+    @Test
     public void testSerialization() {
         final Message message = Message.of("Bukkit is awesome!")
-                .append(new ItemStack(Material.COOKIE,42), ChatColor.RED+"Here your awesome cookie!")
-                .append(Achievement.BAKE_CAKE,ChatColor.UNDERLINE+"Here your awesome cake!")
+                .append(new ItemStack(Material.COOKIE, 42), ChatColor.RED + "Here your awesome cookie!")
+                .append(Achievement.BAKE_CAKE, ChatColor.UNDERLINE + "Here your awesome cake!")
                 .append(Click.ofOpenUrl("http://www.bukkit.org"), "Click here for more information.");
         final Message message2 = Message.deserialize(message.serialize());
         assertEquals(message, message2);
