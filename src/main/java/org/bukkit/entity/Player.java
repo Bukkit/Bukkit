ce341e8a416ee2d323bb23f22e62efcf2eac4260
ST-DDT <ST-DDT@gmx.de>
2014-09-19 23:43:31 +0200

    Added MC 1.8 features

--- src/main/java/org/bukkit/entity/Player.java
+++ src/main/java/org/bukkit/entity/Player.java
@@ -51,8 +51,44 @@ public interface Player extends HumanEntity, Conversable, CommandSender, Offline
      * Gets the name that is shown on the player list.
      *
      * @return the player list name
+     * @deprecated This method now uses {@link Message} to display the name. Use
+     *     {@link #setPlayerListName(Message)} instead.
      */
+    @Deprecated
     public String getPlayerListName();
+
+    /**
+     * Gets the name that is shown on the player list.
+     *
+     * @return the player list name
+     */
+    public Message getPlayerListNameMessage();
+
+    /**
+     * Sets the name that is shown on the in-game player list.
+     * <p>
+     * The name cannot be longer than 16 characters, but {@link ChatColor} is
+     * supported.
+     * <p>
+     * If the value is null, the name will be identical to {@link #getName()}.
+     * <p>
+     * This name is case sensitive and unique, two names with different casing
+     * will appear as two different people. If a player joins afterwards with
+     * a name that conflicts with a player's custom list name, the joining
+     * player's player list name will have a random number appended to it (1-2
+     * characters long in the default implementation). If the joining player's
+     * name is 15 or 16 characters long, part of the name will be truncated at
+     * the end to allow the addition of the two digits.
+     *
+     * @param name new player list name
+     * @throws IllegalArgumentException if the name is already used by someone
+     *     else
+     * @throws IllegalArgumentException if the length of the name is too long
+     * @deprecated This method now uses {@link Message} to send the message. Use
+     *     {@link #setPlayerListName(Message)} instead.
+     */
+    @Deprecated
+    public void setPlayerListName(String name);
 
     /**
      * Sets the name that is shown on the in-game player list.
@@ -75,7 +111,7 @@ public interface Player extends HumanEntity, Conversable, CommandSender, Offline
      *     else
      * @throws IllegalArgumentException if the length of the name is too long
      */
-    public void setPlayerListName(String name);
+    public void setPlayerListName(Message message);
 
     /**
      * Set the target of the player's compass.
